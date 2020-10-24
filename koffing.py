08dbfc875fe3192f6813181a29778c03c14d3941
cheunste <cheunste@gmail.com>
2020-10-24 08:15:18 -0700

    Refactored the sql code in the main call. Now you have an update_zubat_sql function. However, it isn't tested yet

--- koffing.py
+++ koffing.py
@@ -70,7 +70,7 @@ class Koffing:
 		return
 
 	def get_file_paths(self, process_name):
-		return [process.ExecutablePath for process in
+		return [zubat_remote_process.ExecutablePath for zubat_remote_process in
 		        self.get_machine_connection().Win32_Process(Description=f"{process_name}".format(process_name))]
 
 	def reformat_path_to_unc(self, process_file_path):
@@ -99,10 +99,22 @@ class Koffing:
 		return win32serviceutil.QueryServiceStatus(service, self.machine_name)[1] == 4
 
 
+def articuno_check():
+	articuno_local_process = "./articuno.exe"
+	if check_file_exists(articuno_local_process):
+		logging.debug("articuno.exe detected in the folder. Attempting to update Articuno")
+		for sites in get_list_of_articuno_sites_from_file():
+			pass
+		pass
 
-
-def get_list_of_sites_from_file():
+def get_list_of_zubat_sites_from_file():
 	f = open('ZubatUccList.txt', 'r')
+	with(f):
+		site_list = [site.replace("\n", "") for site in f]
+	return site_list
+
+def get_list_of_articuno_sites_from_file():
+	f = open('ArticunoSites.txt', 'r')
 	with(f):
 		site_list = [site.replace("\n", "") for site in f]
 	return site_list
@@ -124,13 +136,39 @@ def sql_file_exists(sql_file):
 def zubat_folders_in_path(path):
 	return [files for files in os.listdir(path) if 'Zubat' in files]
 
+def no_instances_running():
+	logging.error("No Instance of Zubat Running. Attempting to Get the folders from the directory instead.")
+	d_path = fr"//{hostname}/D$/Program Files/IBERINCO"
+	zubat_folders = zubat_folders_in_path(d_path)
+	if(len(zubat_folders) == 0):
+		logging.error(f"No Zubat folders found in {hostname}'s D drive {d_path}. Is Zubat deployed to {hostname}?")
+		logging.error(f"zubat_folders: {zubat_folders}")
+	else:
+		for zubat in zubat_folders:
+			logging.debug(f"Attempting to replace Zubat.exe in {hostname}'s {d_path}")
+			new_file_path = d_path+f"/{zubat}/"
+			koffing.replace_file(f".//{zubat_local_exe}", new_file_path+"Zubat.exe")
+			if parse_sql:
+				logging.debug(f"Updating Database for {hostname}")
+				database_path = f"{new_file_path}Database/ZubatConfiguration.db"
+				logging.debug(f"Updating the database in {database_path}")
+				koffing.update_database(database_path, script_content)
+
+def update_zubat_sql(zubat_file_path,hostname):
+	logging.debug(f"Updating Database for {hostname}")
+	database_path = f"{zubat_file_path}Database/ZubatConfiguration.db"
+	logging.debug(f"Updating the zubat database in {database_path}")
+	koffing.update_database(database_path, script_content)
+
 
 if __name__ == "__main__":
 	logging.basicConfig(filename="koffing.log", level=logging.DEBUG)
-	file_list = get_list_of_sites_from_file()
+	file_list = get_list_of_zubat_sites_from_file()
 	service = "Watchdog"
-	process = "Zubat.exe"
-	file = f".//Zubat.exe"
+	zubat_remote_process = "Zubat.exe"
+	zubat_local_exe = f".//Zubat.exe"
+	articuno_remote_process = "articuno.exe"
+	articuno_local_exe =f".//articuno.exe"
 	sql_script_path = r"./Koffing.sql"
 	sample_packet_size = 50
 	max_acceptable_ping_response = 500
@@ -143,8 +181,8 @@ if __name__ == "__main__":
 		else:
 			logging.debug("Skipping using SQL file")
 
-	if not check_file_exists(file):
-		logging.error(f"{file} does not exist in the current directory, please make sure it is before running.")
+	if not check_file_exists(zubat_local_exe):
+		logging.error(f"{zubat_local_exe} does not exist in the current directory, please make sure it is before running.")
 	else:
 		#Check if SQL file eixsts
 		for hostname in file_list:
@@ -161,21 +199,18 @@ if __name__ == "__main__":
 			koffing.pause_service(service)
 
 			## Get the path of all the processes
-			process_file_path = koffing.get_file_paths(process)
+			process_file_path = koffing.get_file_paths(zubat_remote_process)
 			## Kill all running process
-			koffing.terminate_process(process)
+			koffing.terminate_process(zubat_remote_process)
 			## Replace the Zubat.exe file
 			logging.debug(f"process file paths: {process_file_path}")
 
 			for file_path in process_file_path:
 				logging.debug(f"attempting to replace {file_path}")
 				new_file_path = koffing.reformat_path_to_unc(file_path)
-				koffing.replace_file(f".//{file}", new_file_path)
+				koffing.replace_file(f".//{zubat_local_exe}", new_file_path)
 				if parse_sql:
-					logging.debug(f"Updating Database for {hostname}")
-					database_path = f"{new_file_path[:-9]}\\Database\\ZubatConfiguration.db"
-					logging.debug(f"Updating the database in {database_path}")
-					koffing.update_database(database_path,script_content)
+					update_zubat_sql(new_file_path[:-9],hostname)
 
 			if len(process_file_path) == 0:
 				logging.error("No Instance of Zubat Running. Attempting to Get the folders from the directory instead.")
@@ -188,13 +223,9 @@ if __name__ == "__main__":
 					for zubat in zubat_folders:
 						logging.debug(f"Attempting to replace Zubat.exe in {hostname}'s {d_path}")
 						new_file_path = d_path+f"/{zubat}/"
-						koffing.replace_file(f".//{file}", new_file_path+"Zubat.exe")
+						koffing.replace_file(f".//{zubat_local_exe}", new_file_path+"Zubat.exe")
 						if parse_sql:
-							logging.debug(f"Updating Database for {hostname}")
-							database_path = f"{new_file_path}Database/ZubatConfiguration.db"
-							logging.debug(f"Updating the database in {database_path}")
-							koffing.update_database(database_path, script_content)
-
+							update_zubat_sql(new_file_path,hostname)
 
 			## start back up the watchdog server
 			koffing.resume_service(service)
